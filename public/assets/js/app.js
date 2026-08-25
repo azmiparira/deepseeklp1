5cb26f6789136f0e4f5b23c92d2f88c6783c1092
Azmi Adi Parira <36531787+azmiparira@users.noreply.github.com>
2026-08-25 13:12:24 +0700

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,7 +9,7 @@
 
     // ===== KONFIGURASI =====
     const CONFIG = {
-        GS_API_URL: window.GS_API_URL || 'https://script.google.com/macros/s/AKfycbxBUIV9mEdAX_LXbg_R3g7GMsWc8XErFsKPRKJtt32HBBm_VW687B5o6urHf59mjq75/exec',
+        GS_API_URL: window.GS_API_URL || 'https://script.google.com/macros/s/AKfycbxvgIuyUBegN8U35ld_fU_JugMH0Gu2B3c7UjIYfzxuVyJhbzkKRMBvTotjTINaQKJT/exec',
         WA_ADMIN: '6281932696934',
         HARGA_PER_PCS: 209000,
         BERAT_PER_PCS: 0.15,
